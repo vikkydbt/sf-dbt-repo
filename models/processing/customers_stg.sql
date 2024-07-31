d61a5148c208eaf46649b94cc83221dd191a7988
vikky b <vikkydbt@gmail.com>
2024-07-31 11:56:10 +0000

    checking seed file

---
+++ models/processing/customers_stg.sql
@@ -0,0 +1,13 @@
+SELECT
+CustomerID,
+FirstName,
+LastName,
+Email,
+Phone,
+Address,
+City,
+State,
+ZipCode,
+Updated_at,
+CONCAT(FirstName,LastName) AS CustomerName
+FROM LANDING.CUSTOMERS
---
+++ models/processing/orders_stg.sql
@@ -0,0 +1,13 @@
+SELECT
+OrderID,
+OrderDate,
+CustomerID,
+EmployeeID,
+StoreID,
+Status AS StatusCD,
+CASE
+WHEN Status = '01' THEN 'In Progress' WHEN Status = '02' THEN 'Completed'
+WHEN Status = '03' THEN 'Cancelled' ELSE NULL
+END AS StatusDesc,
+Updated_at
+FROM LANDING.ORDERS
