775e221fc5f5ce88a386cfca58e5f5d83cb82bbe
vikky b <vikkydbt@gmail.com>
2024-08-02 13:49:54 +0000

    incremental use case

--- models/HR/emp.sql
+++ models/HR/emp.sql
@@ -1,1 +1,5 @@
-select *  from employee_details
+{{config(materialized='incremental',unique_key = 'id')}}
+select *  from HR_DB.RAW.EMPLOYEE_DETAILS
+{% if is_incremental() %}
+where LASTUPDATEDATE > (select max(LASTUPDATEDATE) from {{ this }})
+{% endif %}
