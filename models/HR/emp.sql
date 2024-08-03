0e16ff6df8c189f96640f54ae3120aa6b12cd205
vikky b <vikkydbt@gmail.com>
2024-08-03 11:13:08 +0000

    commit emp  script

--- models/HR/emp.sql
+++ models/HR/emp.sql
@@ -1,4 +1,10 @@
-{{config(materialized='incremental',unique_key = 'id')}}
+{{config(materialized='incremental',unique_key = 'id',
+merge_update_columns =['position', 'department']
+
+
+
+
+)}}
 select *  from HR_DB.RAW.EMPLOYEE_DETAILS
 {% if is_incremental() %}
 where LASTUPDATEDATE > (select max(LASTUPDATEDATE) from {{ this }})
